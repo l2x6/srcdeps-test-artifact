66ea95d890531f4eaaa5aa04a9b1c69b409dcd0b
Peter Palaga <ppalaga@redhat.com>
2015-08-23 10:44:24 +0200

    Initial commit

---
+++ src/main/java/org/l2x6/maven/srcdeps/itest/App.java
@@ -0,0 +1,14 @@
+package org.l2x6.maven.srcdeps.itest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private final String message = "Hello World!";
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/test/java/org/l2x6/maven/srcdeps/itest/AppTest.java
@@ -0,0 +1,12 @@
+package org.l2x6.maven.srcdeps.itest;
+
+import org.junit.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    public void testAppMessage() {
+        Assert.assertEquals("Hello World!", new App().getMessage());
+    }
+}
